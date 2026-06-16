12e2bfed3d8079750bddae97342fd9eefad923cb
Juanquirr <juanquirr579@gmail.com>
2026-06-16 12:07:22 +0200

    Internimage

---
+++ InternImage/detection/mmdet_custom/datasets/__init__.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------
+# InternImage
+# Copyright (c) 2022 OpenGVLab
+# Licensed under The MIT License [see LICENSE for details]
+# --------------------------------------------------------
+
+from .crowd_human import CrowdHumanDataset
+from .alpha5 import Alpha5Dataset
+
+__all__ = ['Alpha5Dataset']
